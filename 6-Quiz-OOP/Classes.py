273527409347b51a8795c56634657bb09b6c21af
AyselAydin <ayselaydin01@gmail.com>
2022-07-02 00:45:40 +0300

    Classes.py edited

--- 6-Quiz-OOP/Classes.py
+++ 6-Quiz-OOP/Classes.py
@@ -11,6 +11,31 @@ class Quiz:
   def __init__(self, questions):
     self.questions = questions
     self.score = 0
+    self.index = 0
 
-  def increase_score(self, score):
-    score += 100  
+  def increase_score(self):
+    self.score += 100
+
+  def getQuestion(self):
+    return self.questions[self.index]
+
+  def displayQuestion(self):
+    question = self.getQuestion()
+    print(f"Soru {self.index + 1}: {question.question}")
+
+    for i in question.choices:
+      print("-> " + i)
+
+    answer = input("Cevabı giriniz: ")
+    if question.check_answer(answer):
+      print("Tebrikler, doğru cevap")
+      self.increase_score()
+    else:
+      print(f"Yanlış cevap, doğru cevap: {question.answer}")
+    
+    self.index += 1
+    if len(self.questions) != self.index:
+      self.displayQuestion()
+    else:
+      print(f"Tebrikler, yarışmayı tamamladınız. Toplamda puanınız: {self.score}")
+
