2aaf7dfea3abf29336d1ba1e39556341887f49ff
AyselAydin <ayselaydin01@gmail.com>
2022-07-01 15:44:20 +0300

    Quiz

--- 6-Quiz/Classes.py
+++ 6-Quiz/Classes.py
@@ -6,3 +6,11 @@ class Questions:
 
   def check_answer(self, answer):
     return self.answer == answer
+
+class Quiz:
+  def __init__(self, questions):
+    self.questions = questions
+    self.score = 0
+
+  def increase_score(self, score):
+    score += 100  
