c5455ce2555b28a1f21a5426f827130700cd39cc
AyselAydin <ayselaydin01@gmail.com>
2022-07-01 15:40:26 +0300

    quiz

---
+++ 6-Quiz/Questions_List.py
@@ -0,0 +1,13 @@
+from Classes import Questions as Q
+
+questions_list = [
+                  Q("Albert Einstein hangi alanda Nobel ödülü almıştır?", 
+                            ["Fizik", "Kimya", "Matematik"], 
+                            "Fizik"), 
+                  Q("Kırmızı ve sarı renklerinin karışımından hangi renk elde edilir?", 
+                            ["Mavi", "Yeşil", "Turuncu"], 
+                            "Turuncu"), 
+                  Q("01 plakaya sahip olup bir kapısı cehennem sıcağına çıkan ve insanların bunalıp güneşe ateş attığı ilimiz", 
+                            ["Muş", "Adana", "Çanakkale"], 
+                            "Adana")
+                  ]
